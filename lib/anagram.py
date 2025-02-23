5da73a3c6d39ce8264a46af308b7bfc2dba35c22
Nili Scharf <nilischarf@gmail.com>
2025-02-23 16:53:08 +0200

    complete anagrams lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(self.word)
+    
+    def match(self, words):
+        return [i for i in words if sorted (i.lower()) == self.sorted_word and i.lower() != self.word]
